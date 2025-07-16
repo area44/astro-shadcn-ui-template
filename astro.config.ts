1b5d5870b1ee91d9fb1a4da211a248c28a44760a
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-17 01:16:04 +0700

    clean code

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,15 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+const site = process.env.CI
+  ? process.env.VERCEL_ENV !== "production" && process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "https://astro-shadcn-ui-template.vercel.app"
+  : "http://localhost:4321";
+
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? "https://astro-shadcn-ui-template.vercel.app"
-    : "http://localhost:4321",
+  site,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
